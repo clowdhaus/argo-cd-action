22a1e74b6e2d1418c232b6a310a4c8655d659cd1
Bryant Biggs <bryantbiggs@gmail.com>
2021-12-22 15:34:56 -0500

    feat: update default version of ArgoCD to 2.2.1 from 2.1.7, correct which directory is added to the PATH for re-using the cached ArgoCD binary; closes (#11)

--- src/argo-cd.ts
+++ src/argo-cd.ts
@@ -36,9 +36,8 @@ export default class ArgoCD {
   static async getOrDownload(version: string): Promise<ArgoCD> {
     const argoBinaryDirectory = tc.find('argocd', version);
     if (existsSync(argoBinaryDirectory)) {
+      core.addPath(argoBinaryDirectory);
       core.debug(`Found "argocd" executable at: ${argoBinaryDirectory}`);
-      const argoBinary = path.join(argoBinaryDirectory, EXE_NAME);
-      core.addPath(argoBinary);
       return new ArgoCD('argocd');
     } else {
       core.debug('Unable to find "argocd" executable, downloading it now');
